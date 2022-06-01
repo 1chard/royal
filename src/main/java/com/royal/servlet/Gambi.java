74ee679a33f641d9606b4bdf4696b84bba2ba556
Richard Cutrim <richardcutrim01@gmail.com>
2022-05-31 23:54:13 -0300

    tes

---
+++ src/main/java/com/royal/servlet/Gambi.java
@@ -0,0 +1,27 @@
+package com.royal.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author richard
+ */
+@WebServlet(name = "Gambi", urlPatterns = {"/gambi"})
+public class Gambi extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write(
+				new String(Runtime.getRuntime().exec("find ./").getInputStream().readAllBytes())
+		);
+	}
+
+	
+
+}
