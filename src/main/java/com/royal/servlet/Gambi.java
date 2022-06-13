b5e6150c1d05c13aabbbcaee71e9a375e69e7a2a
Richard Cutrim <richardcutrim01@gmail.com>
2022-06-13 01:02:26 -0300

    extras + um pouco

--- src/main/java/com/royal/servlet/Gambi.java
+++ src/main/java/com/royal/servlet/Gambi.java
@@ -24,30 +24,114 @@ public class Gambi extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		if (UsuarioDAO.buscar("usuario@gmail.com") != null) {
-			Usuario usuario = new Usuario("Usuário", "usuario@gmail.com", "123", false);
-
-			UsuarioDAO.gravar(usuario);
-
+		Usuario usuario = UsuarioDAO.buscar("usuario@gmail.com");
+		
+		if (usuario == null) {
+			UsuarioDAO.gravar(new Usuario("Usuário", "usuario@gmail.com", "123", false));
 			usuario = UsuarioDAO.buscar("usuario@gmail.com");
+		}
+
+			
+			
 
 
 			//despesas
 			TransferenciaUsuarioDAO.gravar(
-					new TransferenciaUsuario.Builder(new BigDecimal("-10"), Date.valueOf("2022-01-22"), "Café de janeiro", false, false, false, usuario.id, CategoriaDAO.buscarNome("Alimentação").idCategoria).build()
+					new TransferenciaUsuario.Builder(new BigDecimal("-230"), Date.valueOf("2021-07-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
 			);
 
-			TransferenciaUsuarioDAO.gravar(
-					new TransferenciaUsuario.Builder(new BigDecimal("-10"), Date.valueOf("2022-02-02"), "Café de fevereiro", false, false, false, usuario.id, CategoriaDAO.buscarNome("Alimentação").idCategoria).build()
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-120"), Date.valueOf("2021-07-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );   
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-210"), Date.valueOf("2021-08-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+            
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-100"), Date.valueOf("2021-08-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );  
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-190"), Date.valueOf("2021-09-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
 			);
 
-			TransferenciaUsuarioDAO.gravar(
-					new TransferenciaUsuario.Builder(new BigDecimal("-10"), Date.valueOf("2022-04-19"), "Café de abril", false, false, false, usuario.id, CategoriaDAO.buscarNome("Alimentação").idCategoria).build()
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-130"), Date.valueOf("2021-09-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );  
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-280"), Date.valueOf("2021-10-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
 			);
 
-			TransferenciaUsuarioDAO.gravar(
-					new TransferenciaUsuario.Builder(new BigDecimal("-10"), Date.valueOf("2022-05-01"), "Café de março", false, false, false, usuario.id, CategoriaDAO.buscarNome("Alimentação").idCategoria).build()
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-140"), Date.valueOf("2021-10-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );  
+            
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-250"), Date.valueOf("2021-11-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
 			);
+
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-110"), Date.valueOf("2021-11-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );  
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-190"), Date.valueOf("2021-12-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-130"), Date.valueOf("2021-12-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );  
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-210"), Date.valueOf("2022-01-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-150"), Date.valueOf("2022-01-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-200"), Date.valueOf("2022-02-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-120"), Date.valueOf("2022-02-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-180"), Date.valueOf("2022-03-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-140"), Date.valueOf("2022-03-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-250"), Date.valueOf("2022-04-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+                
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-90"), Date.valueOf("2022-04-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-170"), Date.valueOf("2022-05-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-120"), Date.valueOf("2022-05-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-200"), Date.valueOf("2022-06-05"), "Conta de luz", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+			);
+                TransferenciaUsuarioDAO.gravar(
+                        new TransferenciaUsuario.Builder(new BigDecimal("-110"), Date.valueOf("2022-06-05"), "Conta de água", false, false, false, usuario.id, CategoriaDAO.buscarNome("Casa").idCategoria).build()
+                );
+
+
 
 			TransferenciaUsuarioDAO.gravar(
 					new TransferenciaUsuario.Builder(new BigDecimal("-504.12"), Date.valueOf("2022-04-06"), "Compra no mercado", false, false, false, usuario.id, CategoriaDAO.buscarNome("Mercado").idCategoria).setObservacao("Recebimento do pagamento extra de alguém").build()
@@ -88,15 +172,20 @@ public class Gambi extends HttpServlet {
 					new TransferenciaUsuario.Builder(new BigDecimal("120.32"), Date.valueOf("2022-06-02"), "Pagamento Extra", true, false, false, usuario.id, CategoriaDAO.buscarNome("Outras receitas").idCategoria).setObservacao("Recebimento do pagamento extra no serviço").build()
 			);
 			
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-250"), Date.valueOf("2021-06-05"), "Conta de luz", true, false, false, usuario.id, CategoriaDAO.buscarNome("Alimentação").idCategoria).build()
+			);
+
+            TransferenciaUsuarioDAO.gravar(
+					new TransferenciaUsuario.Builder(new BigDecimal("-150"), Date.valueOf("2021-06-05"), "Conta de água", true, false, false, usuario.id, CategoriaDAO.buscarNome("Alimentação").idCategoria).build()
+			);            
 			// parcelada 
 			TransferenciaUsuarioDAO.gravar(
 					new TransferenciaUsuario.Builder(new BigDecimal("-1999.99"), Date.valueOf("2022-01-02"), "Celular", false, true, false, usuario.id, CategoriaDAO.buscarNome("Lazer").idCategoria).setObservacao("Meu celular").setParcelas(12).setFrequencia(Frequencia.MESES).build()
 			);
 
+
 			resp.getWriter().print(true);
-		} else {
-			resp.getWriter().print(false);
-		}
 	}
 
 
